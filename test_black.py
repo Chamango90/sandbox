0cc6b95cc110e48a5b9b4c9ad393448b6ca819b0
Jonathan Hechtbauer <jonathan.hechtbauer@ipa.fraunhofer.de>
2018-11-01 10:20:10 +0100

    TEST

--- test_black.py
+++ test_black.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-import sys
-import rospy
-from recordit.recorder import Recorder
+import 	sys
+import 	rospy
+from 		recordit.recorder import Recorder
 
-if __name__ == "__main__":
+if 		__name__ == "__main__":
     rospy.init_node("recordit", anonymous=True)
     record = Recorder()
     rospy.spin()
+
